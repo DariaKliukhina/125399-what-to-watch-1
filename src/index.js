c522c8ed0083295cd871f5ab94a770ed4d7d2728
Daria Kliukhina <dariasergeevna90@gmail.com>
2019-05-05 23:39:14 +0300

    Делает тесты. Убирает props из app

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
 
 const init = () => {
-  const filmsList = [`Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`, `Macbeth`];
   ReactDOM.render(
-      <App filmsList={filmsList}/>,
+      <App/>,
       document.querySelector(`#root`)
   );
 };
