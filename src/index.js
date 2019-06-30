f8cd65912fd6d388864f5f6cae8cc79408bb0dcc
Daria Kliukhina <dariasergeevna90@gmail.com>
2019-06-30 22:42:56 +0300

    Правки

--- src/index.js
+++ src/index.js
@@ -17,8 +17,6 @@ const init = () => {
       reducer,
       compose(
           applyMiddleware(thunk.withExtraArgument(api))
-          , window.__REDUX_DEVTOOLS_EXTENSION__ &&
-          window.__REDUX_DEVTOOLS_EXTENSION__()
       )
   );
 
